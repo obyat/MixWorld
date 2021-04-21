51ba8e55627812a6b5906e0f0a5f19ade1cbfa86
tonychau002 <tony.chau002@umb.edu>
2021-04-20 21:41:10 -0400

    Worked on SnowWorld

---
+++ SnowWorld/Assets/Scripts/FallingFloor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFloor : MonoBehaviour
+{
+    public Vector3 initialPosition;
+    public float dropTimer;
+    public float returnTimer;
+    public bool falling;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+        dropTimer = 10f;
+        speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dropTimer -= Time.deltaTime;
+        if(dropTimer < 0)
+        {
+            dropTimer = 8f;
+            if(Random.Range(0f, 1f) > 0.5f)
+            {
+                falling = true;
+            }
+        }
+        else if (dropTimer < 2 && dropTimer > 0)
+        {
+            transform.position = initialPosition;
+            falling = false;
+        }
+        if(falling)
+        {
+            transform.Translate(Vector3.down* Time.deltaTime * speed);
+        }
+    }
+}
